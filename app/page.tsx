a1f27a7c3d4d8ce5fe77cf6e446396e23abb9aeb
Radicitus <novivolatus@gmail.com>
2023-05-31 16:14:00 -0700

    Add getting stripe products

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,16 @@
-export default function Home() {
+import Stripe from "stripe";
+
+const getProducts = async () => {
+  const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
+    apiVersion: "2022-11-15",
+  });
+
+  const products = await stripe.products.list();
+  console.log(products);
+};
+
+export default async function Home() {
+  const products = await getProducts();
   return (
     <main>
       <div className="text-4xl">Hello world</div>
