6e768406c40f00c17793f8c4d9d9c9cc2ae6e4f3
Radicitus <novivolatus@gmail.com>
2023-06-02 19:27:45 -0700

    Add functions to state management for adding product to cart or updating cart quantity

---
+++ app/product/[id]/AddCart.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+import { useCartStore } from "@/store";
+import { AddCartType } from "@/types/AddCartType";
+
+export default function AddCart({
+  name,
+  image,
+  id,
+  quantity,
+  unit_amount,
+}: AddCartType) {
+  const cartStore = useCartStore();
+
+  return (
+    <>
+      <button
+        onClick={() =>
+          cartStore.addProduct({ name, image, id, quantity, unit_amount })
+        }
+        className="my-12 text-white py-2 px-6 font-medium rounded-md bg-teal-700"
+      >
+        Add to Cart
+      </button>
+    </>
+  );
+}
