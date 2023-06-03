76547715549aaa7b5b1c2c168d2447785a978fc4
Radicitus <novivolatus@gmail.com>
2023-06-02 17:00:24 -0700

    Add cart icon and cart count using state management

--- app/components/Nav.tsx
+++ app/components/Nav.tsx
@@ -6,16 +6,23 @@ import Image from "next/image";
 import Link from "next/link";
 import Cart from "@/app/components/Cart";
 import { useCartStore } from "@/store";
+import { AiFillShopping } from "react-icons/ai";
 
 export default function Nav({ user }: Session) {
   const cartStore = useCartStore();
 
   return (
-    <nav className="flex justify-between items-center py-8">
+    <nav className="flex justify-between items-center py-12">
       <Link href={"/"}>
         <h1>Cam.Shop</h1>
       </Link>
       <ul className="flex items-center gap-12">
+        <li className="flex items-center text-3xl relative cursor-pointer">
+          <AiFillShopping />
+          <span className="bg-teal-700 text-white text-sm font-bold w-5 h-5 rounded-full absolute left-4 bottom-4 flex items-center justify-center">
+            {cartStore.cart.length}
+          </span>
+        </li>
         {/* If the user is not signed in */}
         {!user && (
           <li className="bg-teal-600 text-white py-2 px-4 rounded-md">
