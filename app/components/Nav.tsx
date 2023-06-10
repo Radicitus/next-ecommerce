b8d918927807f9fc0d4293451c523a040f288489
Radicitus <novivolatus@gmail.com>
2023-06-09 19:24:03 -0700

    Add dropdown to nav for orders and signout

--- app/components/Nav.tsx
+++ app/components/Nav.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { Session } from "next-auth";
-import { signIn } from "next-auth/react";
+import { signIn, signOut } from "next-auth/react";
 import Image from "next/image";
 import Link from "next/link";
 import Cart from "@/app/components/Cart";
@@ -47,17 +47,45 @@ export default function Nav({ user }: Session) {
 
         {/* If the user is signed in */}
         {user && (
-          <Link href={"/dashboard"}>
-            <li>
+          <li>
+            <div className="dropdown dropdown-end cursor-pointer">
               <Image
                 src={user?.image as string}
                 alt={user?.name as string}
                 width={48}
                 height={48}
                 className="rounded-full"
-              ></Image>
-            </li>
-          </Link>
+                tabIndex={0}
+              />
+              <ul
+                tabIndex={0}
+                className="dropdown-content menu p-4 space-y-4 shadow bg-base-100 rounded-box w-72"
+              >
+                <Link
+                  className="hover:bg-base-300 p-4 rounded-md"
+                  href={"/dashboard"}
+                  onClick={() => {
+                    if (document.activeElement instanceof HTMLElement) {
+                      document.activeElement.blur();
+                    }
+                  }}
+                >
+                  Orders
+                </Link>
+                <li
+                  className="hover:bg-base-300 p-4 rounded-md"
+                  onClick={() => {
+                    signOut();
+                    if (document.activeElement instanceof HTMLElement) {
+                      document.activeElement.blur();
+                    }
+                  }}
+                >
+                  Sign Out
+                </li>
+              </ul>
+            </div>
+          </li>
         )}
       </ul>
       <AnimatePresence>{cartStore.isOpen && <Cart />}</AnimatePresence>
