1e4e4ae43e5b84aaf22f1bfcd2defffcc1cf162e
Radicitus <novivolatus@gmail.com>
2023-06-12 13:42:13 -0700

    Refactor getProducts to a util function to clean up home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,33 +1,6 @@
-import Stripe from "stripe";
 import Product from "@/app/components/Product";
 import { Analytics } from "@vercel/analytics/react";
-
-const getProducts = async () => {
-  const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
-    apiVersion: "2022-11-15",
-  });
-
-  const products = await stripe.products.list();
-
-  return await Promise.all(
-    products.data.map(async (product) => {
-      const prices = await stripe.prices.list({
-        product: product.id,
-      });
-      const features = product.metadata.features || "";
-
-      return {
-        id: product.id,
-        name: product.name,
-        unit_amount: prices.data[0].unit_amount,
-        image: product.images[0],
-        currency: prices.data[0].currency,
-        description: product.description,
-        metadata: { features },
-      };
-    })
-  );
-};
+import getProducts from "@/util/GetProducts";
 
 export default async function Home() {
   const products = await getProducts();
