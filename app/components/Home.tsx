a7e0fe14591844442aadc1b390064cf670bb28bf
Radicitus <novivolatus@gmail.com>
2023-06-13 16:53:05 -0700

    Fetch and set products on component load

--- app/components/Home.tsx
+++ app/components/Home.tsx
@@ -1,24 +1,37 @@
 "use client";
 
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import { ProductType } from "@/types/ProductType";
+import Product from "@/app/components/Product";
+
+const fetchProducts = async () => {
+  const res = await fetch("/api/get-products");
+  return await res.json();
+};
 
 export default function Home() {
-  const fetchProducts = async () => {
-    const res = await fetch("/api/get-products");
-    return await res.json();
-  };
+  const [products, setProducts] = useState<ProductType[]>([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     fetchProducts().then((data) => {
-      console.log(data);
+      setProducts(data);
+      setLoading(false);
     });
-  });
+  }, []);
+
+  if (loading)
+    return (
+      <div className="h-full flex justify-center content-center">
+        <span className="loading loading-ring loading-lg"></span>
+      </div>
+    );
 
   return (
     <div className="flex flex-wrap gap-8 lg:gap-16 pb-12 justify-center">
-      {/*{products.map((product) => (*/}
-      {/*  <Product {...product} key={product.id} />*/}
-      {/*))}*/}
+      {products.map((product) => (
+        <Product {...product} key={product.id} />
+      ))}
     </div>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Home from "@/app/components/Home";
 
 export default async function Main() {
   return (
-    <main>
+    <main className="h-full w-full">
       <Home />
       <Analytics />
     </main>
